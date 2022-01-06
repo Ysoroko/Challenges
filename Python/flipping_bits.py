5c6df1c42772229db27dcaeabc4deb19afdf1f38
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-01-06 15:13:30 +0100

    Create flipping_bits.py

---
+++ Python/flipping_bits.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    b = (str(bin(n)[2:])).zfill(32)
+    ret = 0
+    for i, char in enumerate(b):
+        if char == "0":
+            ret += 2 ** (32 - i - 1)
+    return (ret)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
