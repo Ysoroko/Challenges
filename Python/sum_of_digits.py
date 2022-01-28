969f958655c375c045be2e3c22b8e9fd63c70364
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-01-28 09:48:55 +0100

    Create sum_of_digits.py

---
+++ Python/sum_of_digits.py
@@ -0,0 +1,10 @@
+#     16  -->  1 + 6 = 7
+#    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+# 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+def digital_root(n):
+    if len(str(n)) == 1:
+        return n
+    s = str(n)
+    return digital_root(sum([int(digit) for digit in s]))
