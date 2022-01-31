dbd97b747b7fdf87e95a70b388fa6888c54831cf
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-01-31 11:57:18 +0100

    Create equalize_the_array.py

---
+++ Python/equalize_the_array.py
@@ -0,0 +1,34 @@
+# Hackerrank challenge
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    c = Counter(arr)
+    m = max(c.values())
+    return (len(arr) - m)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
