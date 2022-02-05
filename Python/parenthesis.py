b13b6d1064eaef336ee53bc09040e7b37fdeda6d
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-02-05 21:28:22 +0100

    Create parenthesis.py

---
+++ Python/parenthesis.py
@@ -0,0 +1,34 @@
+# Codingame problem :
+# Check if the parenthesis are closed in input
+
+parenthesis = {
+		"(" : ")",
+		"{" : "}",
+		"[" : "]"
+	}
+
+def	get_corresponding_key(value):
+	return list(parenthesis.keys())[list(parenthesis.values()).index(value)]
+
+if __name__ == "__main__":
+	line = input()
+
+	stack = list()
+	for char in line:
+		if char in parenthesis.keys():
+			stack.append(char)
+		if char in parenthesis.values():
+			if not len(stack):
+				break
+			else:
+				if stack[-1] != get_corresponding_key(char):
+					break
+				else:
+					# By default without the index argument given, the argument defaults to -1
+					stack.pop()
+
+	d = {
+	    True : "Not Balanced",
+	    False : "Balanced" 
+	}
+	print(d[len(stack) != 0])
