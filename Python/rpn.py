9bf4c9c20f4f6bf6a808bfef84c1aab32f062856
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-02-12 08:59:59 +0100

    Create rpn.py

---
+++ Python/rpn.py
@@ -0,0 +1,23 @@
+# Hackerrank challenge
+# Reverse polish notiation
+# Example: ["2", "1", "+", "3", "*"] is equal to (2 + 1) * 3 , should output "6"
+
+d = {
+	'+' : lambda x, y : x + y,
+	'-' : lambda x, y : x - y,
+	'*' : lambda x, y : x * y,
+	'/' : lambda x, y : x // y
+}
+
+if __name__ == "__main__":
+	_input = input().split()
+	stack = list()
+	for elem in _input:
+		if elem.isdigit():
+			stack.append(elem)
+		else:
+			b = int(stack.pop())
+			a = int(stack.pop())
+			res = d[elem](a, b)
+			stack.append(res)
+	print(stack.pop())
